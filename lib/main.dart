78b8777f2b9657574268b53f3225d52ba43873f6
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-19 19:16:45 +0330

    +routes an login navigation

--- lib/main.dart
+++ lib/main.dart
@@ -5,57 +5,31 @@ import 'package:kip/widgets/NoteItem.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: KipMainPage(),
+      routes: <String, WidgetBuilder>{
+        '/home': (BuildContext inContext) => KipMainPage(),
+//        '/login': (BuildContext inContext)=> LoginPage(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class KipMainPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _KipMainPageState createState() => _KipMainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
+class _KipMainPageState extends State<KipMainPage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return SafeArea(
       child: Scaffold(
         bottomNavigationBar: BottomAppBar(
@@ -83,23 +57,11 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
         body: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            KipBar(),
+            KipBar(
+              onRequestLogin: onNavigateToLogin(context),
+            ),
             Expanded(
               child: ListView(
                 children: <Widget>[
@@ -131,10 +93,14 @@ class _MyHomePageState extends State<MyHomePage> {
           onPressed: addNote,
           tooltip: 'Increment',
           child: Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
+        ),
       ),
     );
   }
 
   addNote() {}
+
+  onNavigateToLogin(BuildContext context) {
+//    Navigator.push(context, );
+  }
 }
