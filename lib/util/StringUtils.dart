01417983536e1f94a2f76ef9428a84275c873625
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-19 20:26:03 +0330

    adding the actual email validation

--- lib/util/StringUtils.dart
+++ lib/util/StringUtils.dart
@@ -1,7 +1,7 @@
 class StringUtils {
 
   static bool isValidEmail(String str){
-    RegExp exp = new RegExp(r"(\w+)");
+    RegExp exp = new RegExp(r"\w{2,}@\w+\.\w{2,}");
     return exp.hasMatch(str);
   }
 }
