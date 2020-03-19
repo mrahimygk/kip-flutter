412d2d46977b73185b89f9e3e119660134547e31
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-19 12:49:18 +0330

    c+a+l

--- lib/widgets/NoteItem.dart
+++ lib/widgets/NoteItem.dart
@@ -2,15 +2,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kip/widgets/BorderedContainer.dart';
 
-class NoteItem extends StatelessWidget{
+class NoteItem extends StatelessWidget {
   final Widget child;
 
   const NoteItem({Key key, this.child}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Padding(
-      padding: const EdgeInsets.only(left:8.0, right:8.0, top:8.0, bottom:0.0),
+      padding:
+          const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0, bottom: 0.0),
       child: BorderedContainer(child: this.child),
     );
   }
