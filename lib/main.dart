8e91c0f90b2923bcc934ea1e91f8d4570bc90abc
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-19 19:19:16 +0330

    separating main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kip/widgets/KipBar.dart';
-import 'package:kip/widgets/NoteItem.dart';
+import 'package:kip/pages/KipMainPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,88 +18,5 @@ class MyApp extends StatelessWidget {
 //        '/login': (BuildContext inContext)=> LoginPage(),
       },
     );
-  }
-}
-
-class KipMainPage extends StatefulWidget {
-  @override
-  _KipMainPageState createState() => _KipMainPageState();
-}
-
-class _KipMainPageState extends State<KipMainPage> {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        bottomNavigationBar: BottomAppBar(
-          shape: CircularNotchedRectangle(),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.check_box),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.aspect_ratio),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.mic),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.photo),
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            KipBar(
-              onRequestLogin: onNavigateToLogin(context),
-            ),
-            Expanded(
-              child: ListView(
-                children: <Widget>[
-                  NoteItem(
-                    child: ListTile(leading: Text("ffff")),
-                  ),
-                  NoteItem(
-                    child: ListTile(leading: Text("ffff")),
-                  ),
-                  NoteItem(
-                    child: ListTile(leading: Text("ffff")),
-                  ),
-                  NoteItem(
-                    child: ListTile(leading: Text("ffff")),
-                  ),
-                  NoteItem(
-                    child: ListTile(leading: Text("ffff")),
-                  ),
-                  NoteItem(
-                    child: ListTile(leading: Text("ffff")),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-        floatingActionButton: FloatingActionButton(
-          onPressed: addNote,
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
-        ),
-      ),
-    );
-  }
-
-  addNote() {}
-
-  onNavigateToLogin(BuildContext context) {
-//    Navigator.push(context, );
   }
 }
