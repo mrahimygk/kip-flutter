a7adc3daffe10b8f35974fb29336a1a6beebbd6a
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-19 19:15:20 +0330

    +user and login callback

--- lib/widgets/ProfileWidget.dart
+++ lib/widgets/ProfileWidget.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kip/models/User.dart';
 
 class ProfileWidget extends StatelessWidget {
+  final User user;
+  final VoidCallback onPress;
+
+  const ProfileWidget({Key key, this.user, this.onPress}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return CircleAvatar(
-        backgroundImage: NetworkImage(
-            "https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-600w-1029171697.jpg"));
+    return user == null
+        ? IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () {
+              onPress();
+            },
+          )
+        : CircleAvatar(
+            backgroundImage: NetworkImage(user.avatar),
+          );
   }
 }
