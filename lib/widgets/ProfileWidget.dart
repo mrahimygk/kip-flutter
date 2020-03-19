0a00930305783224523f889692cf30f808ab8779
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-19 11:02:41 +0330

    using the better widget for user avatar

--- lib/widgets/ProfileWidget.dart
+++ lib/widgets/ProfileWidget.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class ProfileWidget extends StatelessWidget{
+class ProfileWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      width: 32.0,
-      height: 32.0,
-      child: ClipOval(
-        child: Image.network("https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-600w-1029171697.jpg"),
-      ),
-    );
+    return CircleAvatar(
+        backgroundImage: NetworkImage(
+            "https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-600w-1029171697.jpg"));
   }
 }
