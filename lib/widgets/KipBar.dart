1bdfd709cb55c84e5570fd8200e76c6c7cd18d8f
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-19 10:50:48 +0330

    +border

--- lib/widgets/KipBar.dart
+++ lib/widgets/KipBar.dart
@@ -10,31 +10,36 @@ class KipBar extends StatefulWidget {
 class KipBarState extends State<KipBar> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(4.0),
       child: Container(
-        child: Row(
-          children: <Widget>[
-            Icon(Icons.menu),
-            Flexible(child: Padding(
-              padding: const EdgeInsets.only(left: 4.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Search in notes"
+        decoration: BoxDecoration(
+            border: Border.all(color: Color(0x55555555)),
+            borderRadius: BorderRadius.all(Radius.circular(8.0))),
+        child: Padding(
+          padding: const EdgeInsets.only(
+              top: 4.0, bottom: 4.0, left: 8.0, right: 8.0),
+          child: Row(
+            children: <Widget>[
+              Icon(Icons.menu),
+              Flexible(
+                  child: Padding(
+                padding: const EdgeInsets.only(left: 4.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none, hintText: "Search in notes"),
                 ),
+              )),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Icon(Icons.border_all),
               ),
-            )),
-            Padding(
-              padding: const EdgeInsets.only(left:8.0),
-              child: Icon(Icons.border_all),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left:8.0),
-              child: ProfileWidget(),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: ProfileWidget(),
+              )
+            ],
+          ),
         ),
       ),
     );
