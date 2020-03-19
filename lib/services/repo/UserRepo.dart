7539bbceb26092a85885bfdbf6cb75f947f55149
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-19 16:05:39 +0330

    +user repo

---
+++ lib/services/repo/UserRepo.dart
@@ -0,0 +1,22 @@
+import 'package:kip/models/User.dart';
+import 'package:kip/services/db/DatabaseProvider.dart';
+
+abstract class UserRepo {
+  DatabaseProvider databaseProvider;
+
+  Future insert(User data);
+
+  Future update(User data);
+
+  Future delete(User data);
+
+  Future<User> getFromDb();
+
+  /// login with email and pass
+  /// returns user with avatar
+  Future<User> login(String email, String pass);
+
+  /// sign up with email and pass
+  /// returns user with random avatar
+  Future<User> signUp(String email, String pass);
+}
