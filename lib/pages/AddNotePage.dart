5037d4c65da434fc315825635870d540ee72d596
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-21 18:49:04 +0430

    +top bar icon buttons

--- lib/pages/AddNotePage.dart
+++ lib/pages/AddNotePage.dart
@@ -13,6 +13,37 @@ class _AddNotePageState extends State<AddNotePage> {
     //TODO: get predefined data here: flag for added default checkboxes...
     return SafeArea(
       child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(48),
+          child: Stack(children: <Widget>[
+            Align(
+              alignment: Alignment.topLeft,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: Icon(Icons.arrow_back)),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  IconButton(icon: Icon(Icons.favorite_border), onPressed: (){},),
+                  IconButton(icon: Icon(Icons.timer), onPressed: (){},),
+                  IconButton(icon: Icon(Icons.archive), onPressed: (){},),
+                ],
+              ),
+            )
+          ]),
+        ),
         key: _scaffoldKey,
         body: SingleChildScrollView(
           child: Column(
