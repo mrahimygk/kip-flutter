84d36cb2f83b4520c3c6e6e0b199cc111786a705
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-21 14:26:27 +0430

    +user api to use base api

---
+++ lib/services/network/api/UserApi.dart
@@ -0,0 +1,13 @@
+import 'BaseApi.dart';
+
+class UserApi {
+  final BaseApi api = BaseApi();
+
+  Future<dynamic> login(String path, Map<String, dynamic> params) async {
+    return api.post(path, params);
+  }
+
+  Future<dynamic> register(String path, Map<String, dynamic> params) async {
+    return api.post(path, params);
+  }
+}
