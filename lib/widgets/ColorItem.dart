600b8f3bcb769f33b4e95b20f1638151b19a3a52
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-22 05:38:06 +0430

    +colored item

---
+++ lib/widgets/ColorItem.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class ColorItem extends StatelessWidget {
+  final Color itemColor;
+
+  const ColorItem({Key key, this.itemColor}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 24,
+        width: 24,
+        decoration: BoxDecoration(
+          color: itemColor,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+}
