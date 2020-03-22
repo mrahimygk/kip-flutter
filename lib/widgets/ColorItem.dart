8d43d5c956c94f8aa9787fa254053a598a3de6ca
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-22 18:10:31 +0430

    +size for items

--- lib/widgets/ColorItem.dart
+++ lib/widgets/ColorItem.dart
@@ -4,19 +4,23 @@ import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
   final Color itemColor;
+  double size;
 
-  const ColorItem({Key key, this.itemColor}) : super(key: key);
+  ColorItem({Key key, this.itemColor, this.size = 24.0}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: 24,
-        width: 24,
-        decoration: BoxDecoration(
-          color: itemColor,
-          shape: BoxShape.circle,
+    return InkWell(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          height: size,
+          width: size,
+          decoration: BoxDecoration(
+            color: itemColor,
+            shape: BoxShape.circle,
+          ),
         ),
       ),
     );
