51d280ea4267f8ac69d96cf1410a012336bcbee1
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-22 05:38:14 +0430

    -

--- lib/widgets/ColorItem.dart
+++ lib/widgets/ColorItem.dart
@@ -9,7 +9,6 @@ class ColorItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
