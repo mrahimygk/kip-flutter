71def4cf0f78adb5148697184c940540b61fc330
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-22 18:18:34 +0430

    getting size from caller, +onPress callback

--- lib/widgets/ColorItem.dart
+++ lib/widgets/ColorItem.dart
@@ -4,14 +4,17 @@ import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
   final Color itemColor;
-  double size;
+  final double size;
+  final VoidCallback onPress;
 
-  ColorItem({Key key, this.itemColor, this.size = 24.0}) : super(key: key);
+  ColorItem({Key key, this.itemColor, this.onPress, this.size = 24.0}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        onPress();
+      },
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Container(
