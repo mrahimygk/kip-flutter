e95e0bcef60961bb73d89c5694c9b46118b06181
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-22 20:51:46 +0430

    +stack for brush size bg

--- lib/widgets/ColorItem.dart
+++ lib/widgets/ColorItem.dart
@@ -7,24 +7,38 @@ class ColorItem extends StatelessWidget {
   final double size;
   final VoidCallback onPress;
 
-  ColorItem({Key key, this.itemColor, this.onPress, this.size = 24.0}) : super(key: key);
+  ColorItem({Key key, this.itemColor, this.onPress, this.size = 24.0})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTapUp: (d) {
+    return InkWell(
+      onTap:(){
         onPress();
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          height: size,
-          width: size,
-          decoration: BoxDecoration(
-            color: itemColor,
-            shape: BoxShape.circle,
+      } ,
+      child: Stack(
+        children: <Widget>[
+          ///grey bg
+          Container(
+            width: 48,
+            height: 48,
           ),
-        ),
+
+          ///actual circle
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.center,
+              child: Container(
+                height: size,
+                width: size,
+                decoration: BoxDecoration(
+                  color: itemColor,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
