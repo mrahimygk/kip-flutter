70f3ffece7f5469c5614101c4aa15d57292eae79
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-25 21:21:48 +0430

    +child to get the width from

--- lib/widgets/RecordingBars.dart
+++ lib/widgets/RecordingBars.dart
@@ -2,10 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RecordingBars extends StatelessWidget {
+  final Widget child;
+
+  const RecordingBars({Key key, this.child}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
       painter: BarsPainter(),
+      child: child,
     );
   }
 }
