4f3ec71f84c9aef19096bddcf01d1e8390e3c5d2
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-25 19:12:55 +0430

    +millis to timer item widget

--- lib/widgets/TimerItem.dart
+++ lib/widgets/TimerItem.dart
@@ -3,22 +3,23 @@ import 'package:intl/intl.dart';
 
 class TimerItem extends StatelessWidget {
   final Duration recordedDuration;
+  final int millis;
 
-  TimerItem(this.recordedDuration);
+  TimerItem(this.recordedDuration, this.millis);
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisSize: MainAxisSize.max,
       children: <Widget>[
-        Text(recordedDuration.toString())
-//        Text(recordedDuration.inHours.toString().padLeft(2,'0')),
-//        Text(":"),
-//        Text((recordedDuration.inMinutes % 60).toString().padLeft(2,'0')),
-//        Text(":"),
-//        Text((recordedDuration.inSeconds % 60).toString().padLeft(2,'0')),
-//        Text("."),
-//        Text((recordedDuration.inSeconds.toDouble() % 1000.0).toString().padLeft(3,'0')),
+//        Text(recordedDuration.toString())
+        Text(recordedDuration.inHours.toString().padLeft(2,'0')),
+        Text(":"),
+        Text((recordedDuration.inMinutes % 60).toString().padLeft(2,'0')),
+        Text(":"),
+        Text((recordedDuration.inSeconds % 60).toString().padLeft(2,'0')),
+        Text("."),
+        Text(millis.toString().padLeft(3,'0')),
       ],
     );
   }
