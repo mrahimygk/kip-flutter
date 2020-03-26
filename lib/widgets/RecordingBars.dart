6b5f6499189ba6ff6e127a53d8aeb8a583c920df
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-26 10:24:17 +0430

    +better matrix transformation

--- lib/widgets/RecordingBars.dart
+++ lib/widgets/RecordingBars.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:matrix4_transform/matrix4_transform.dart';
 
 class RecordingBars extends StatelessWidget {
   final Widget child;
@@ -18,20 +19,18 @@ class RecordingBars extends StatelessWidget {
 class BarsPainter extends CustomPainter {
   final Paint painter = Paint()
     ..color = Colors.blue
-    ..strokeWidth = 4;
+    ..strokeWidth = 2;
 
   final int pointCount = 66;
 
   @override
   void paint(Canvas canvas, Size size) {
     canvas.drawLine(Offset.zero, Offset(size.width, size.height), painter);
-    canvas.translate(size.width / 2, size.height / 2);
-    final rad = size.height / 8.0;// - 18.0*2;
+    final rad = size.height / 2.25;// - 18.0*2;
     for (double i = 0; i < 360; i = i + (360 / pointCount)) {
       canvas.save();
-//      canvas.transform(matrix4);
-      canvas.rotate(-90);
-      canvas.rotate(-i);
+      canvas.transform(Matrix4Transform().rotateByCenterDegrees(-90, size).m.storage);
+      canvas.transform(Matrix4Transform().rotateByCenterDegrees(-i, size).m.storage);
       final cx = size.width / 2.0 + rad;
       final cy = size.height / 2.0;
       canvas.drawLine(Offset(cx, cy), Offset(cx + 10, cy), painter);
