601399eec11dd55716999e1f9ac7c95b7fa25fc2
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-26 11:22:06 +0430

    painting bars by random amplitudes

--- lib/widgets/RecordingBars.dart
+++ lib/widgets/RecordingBars.dart
@@ -1,16 +1,27 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:matrix4_transform/matrix4_transform.dart';
 
 class RecordingBars extends StatelessWidget {
   final Widget child;
+  final double maxLineLength;
+  final double audioPeak;
+  final List<int> thresholds;
 
-  const RecordingBars({Key key, this.child}) : super(key: key);
+  const RecordingBars(
+      {Key key,
+      this.child,
+      this.maxLineLength,
+      this.audioPeak,
+      this.thresholds})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
-      painter: BarsPainter(),
+      painter: BarsPainter(maxLineLength, audioPeak, thresholds),
       child: child,
     );
   }
@@ -22,18 +33,37 @@ class BarsPainter extends CustomPainter {
     ..strokeWidth = 2;
 
   final int pointCount = 66;
+  final double maxLineLength;
+  final double audioPeak;
+  final List<int> thresholds;
+  final points = List();
+
+  BarsPainter(this.maxLineLength, this.audioPeak, this.thresholds) {
+    final random = Random();
+
+    for (int i = 1; i <= pointCount; i++) {
+      points.add(audioPeak / (random.nextInt(thresholds[i - 1]) + 1));
+    }
+  }
 
   @override
   void paint(Canvas canvas, Size size) {
     canvas.drawLine(Offset.zero, Offset(size.width, size.height), painter);
     final rad = size.height / 2.25; // - 18.0*2;
-    for (double i = 0; i < 360; i = i + (360 / pointCount)) {
+    for (int i = 0; i < 360; i = i + (360 / pointCount).floor()) {
+      int index = (i * pointCount / 360).round();
+      var peakValue = points[index];
+      if (peakValue > maxLineLength) peakValue = maxLineLength;
       canvas.save();
-      canvas.transform(Matrix4Transform().rotateByCenterDegrees(-90, size).m.storage);
-      canvas.transform(Matrix4Transform().rotateByCenterDegrees(-i, size).m.storage);
+      canvas.transform(
+          Matrix4Transform().rotateByCenterDegrees(-90, size).m.storage);
+      canvas.transform(Matrix4Transform()
+          .rotateByCenterDegrees(-i.toDouble(), size)
+          .m
+          .storage);
       final cx = size.width / 2.0 + rad;
       final cy = size.height / 2.0;
-      canvas.drawLine(Offset(cx, cy), Offset(cx + 10, cy), painter);
+      canvas.drawLine(Offset(cx, cy), Offset(cx + peakValue, cy), painter);
       canvas.restore();
     }
   }
