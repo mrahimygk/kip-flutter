10b5528b167ff84dc0f25eff8895d423d5e2fb35
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-26 10:40:13 +0430

    trying to add a matrix4 util to rotate by centre

---
+++ lib/util/MatrixUtils.dart
@@ -0,0 +1,10 @@
+//import 'dart:math';
+//import 'package:flutter/rendering.dart';
+//
+//class MatrixUtils {
+//  final Matrix4 m;
+//  MatrixUtils() : m = Matrix4.identity();
+
+//  Matrix4 rotateByCenterDegrees(double angleDegrees, Size size) =>
+//      rotateByCenter(_toRadians(angleDegrees), size);
+//}
