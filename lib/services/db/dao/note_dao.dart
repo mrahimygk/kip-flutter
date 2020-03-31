9313df3f5a320c96520e175e213f6db6464e0dfe
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-31 19:44:58 +0430

    +dao for notes

---
+++ lib/services/db/dao/note_dao.dart
@@ -0,0 +1,67 @@
+import 'package:kip/models/note/note_model.dart';
+
+import 'dao.dart';
+
+class NoteDao implements Dao<NoteModel> {
+  @override
+  String get createTableQuery => 'create table $noteTable ('
+      '$noteColumnId text primary key,'
+      '$noteColumnTitle text, '
+      '$noteColumnContent text, '
+      '$noteColumnColor text, '
+      '$noteColumnDrawingList text, '
+      '$noteColumnVoiceList text, '
+      '$noteColumnCheckboxList text, '
+      '$noteColumnLabelList text, '
+      '$noteColumnIsPinned BOOLEAN NOT NULL CHECK ($noteColumnIsPinned IN (0,1) '
+      ')';
+
+  @override
+  Map<String, dynamic> toMap(NoteModel object) {
+    return <String, dynamic>{
+      noteColumnId: object.id,
+      noteColumnTitle: object.title,
+      noteColumnContent: object.content,
+      noteColumnColor: object.color,
+      noteColumnDrawingList: object.drawingList,
+      noteColumnVoiceList: object.voiceList,
+      noteColumnCheckboxList: object.checkboxList,
+      noteColumnLabelList: object.labelList,
+      noteColumnIsPinned: object.isPinned
+    };
+  }
+
+  @override
+  List<NoteModel> fromList(List<Map<String, dynamic>> query) {
+    List<NoteModel> res = List<NoteModel>();
+    for (Map map in query) {
+      res.add(fromMap(map));
+    }
+
+    return res;
+  }
+
+  @override
+  NoteModel fromMap(Map<String, dynamic> query) => NoteModel(
+        query[noteColumnId],
+        query[noteColumnTitle],
+        query[noteColumnContent],
+        query[noteColumnColor],
+        query[noteColumnDrawingList],
+        query[noteColumnVoiceList],
+        query[noteColumnCheckboxList],
+        query[noteColumnLabelList],
+        query[noteColumnIsPinned],
+      );
+}
+
+const noteTable = 'note';
+const noteColumnId = 'id';
+const noteColumnTitle = 'title';
+const noteColumnContent = 'content';
+const noteColumnColor = 'color';
+const noteColumnDrawingList = 'drawing_list';
+const noteColumnVoiceList = 'voice_list';
+const noteColumnCheckboxList = 'checkbox_list';
+const noteColumnLabelList = 'label_list';
+const noteColumnIsPinned = 'is_linned';
