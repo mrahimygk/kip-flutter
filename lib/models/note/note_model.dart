914c3102748b3c2164bb84f3d1151cc63695ca48
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-31 17:15:15 +0430

    +fun to map from/to json

--- lib/models/note/note_model.dart
+++ lib/models/note/note_model.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
+import 'package:kip/util/ext/color.dart';
 
 import 'checkbox_model.dart';
 
@@ -15,6 +16,36 @@ class NoteModel {
   final List<String> labelList;
   bool isPinned;
 
-  NoteModel(this.id,this.content, this.title, this.color, this.drawingList, this.voiceList,
-      this.checkboxList, this.labelList, this.isPinned);
+  NoteModel(this.id, this.title, this.content, this.color, this.drawingList,
+      this.voiceList, this.checkboxList, this.labelList, this.isPinned);
+
+  factory NoteModel.fromMap(Map<String, dynamic> json) {
+//    final checkboxList = List<CheckboxModel>();
+
+    return NoteModel(
+        json['id'],
+        json['title'],
+        json['content'],
+        HexColor.fromHex(json['color']),
+        json['drawing_list'],
+        json['voice_list'],
+        json['checkbox_list'],
+//        CheckboxModel.fromMap(json['checkbox_list']),
+        json['label_list'],
+        json['is_pinned']);
+  }
+
+  Map<String, dynamic> toMap() {
+    final data = Map<String, dynamic>();
+    data['id'] = id;
+    data['title'] = title;
+    data['content'] = content;
+    data['drawing_list'] = drawingList;
+    data['voice_list'] = voiceList;
+    data['checkbox_list'] = checkboxList;
+    data['label_list'] = labelList;
+    data['is_pinned'] = isPinned;
+    data['content'] = color.toHex();
+    return data;
+  }
 }
