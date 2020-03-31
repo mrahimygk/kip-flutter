cec2e2b51a0c76b993bcc2e82d3be25d0b60a391
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-31 14:16:23 +0430

    todo to remember adding the editing note

--- lib/models/add_note_page_arguments.dart
+++ lib/models/add_note_page_arguments.dart
@@ -2,5 +2,6 @@ class AddNotePageArguments {
   final bool shouldAddDrawing;
   final bool shouldAddCheckboxes;
 
+  //TODO: + editing note
   AddNotePageArguments(this.shouldAddDrawing, this.shouldAddCheckboxes);
 }
