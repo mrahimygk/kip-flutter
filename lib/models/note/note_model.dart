af1f0e55333f22ce499c54b29f63d250c91fcfed
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-31 14:16:45 +0430

    +content to note

--- lib/models/note/note_model.dart
+++ lib/models/note/note_model.dart
@@ -7,6 +7,7 @@ import 'checkbox_model.dart';
 class NoteModel {
   final String id;
   String title;
+  String content;
   Color color;
   final List<String> drawingList;
   final List<String> voiceList;
@@ -14,6 +15,6 @@ class NoteModel {
   final List<String> labelList;
   bool isPinned;
 
-  NoteModel(this.id, this.title, this.color, this.drawingList, this.voiceList,
+  NoteModel(this.id,this.content, this.title, this.color, this.drawingList, this.voiceList,
       this.checkboxList, this.labelList, this.isPinned);
 }
