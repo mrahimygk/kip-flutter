b61d73135505393233411c53d100aacb82da7d7f
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-11 20:56:57 +0430

    fixing cast

--- lib/util/ext/SnackBar.dart
+++ lib/util/ext/SnackBar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 extension CustomSnackBar on GlobalKey {
   void showRowSnackBar(Widget child) {
-    (this as ScaffoldState).showSnackBar(
+    (this.currentState as ScaffoldState).showSnackBar(
       SnackBar(
         content: Row(
           children: <Widget>[
