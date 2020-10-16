2b43785cf81b70aba749f71c945caa5b7b52c539
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-10-16 09:40:49 +0330

    adding new image path to args

--- lib/models/add_note_page_arguments.dart
+++ lib/models/add_note_page_arguments.dart
@@ -3,8 +3,14 @@ import 'package:kip/models/note/note_model.dart';
 class AddNotePageArguments {
   final bool shouldAddDrawing;
   final bool shouldAddCheckboxes;
+  final String imagePath;
   final NoteModel note;
 
   //TODO: + editing note
-  AddNotePageArguments(this.shouldAddDrawing, this.shouldAddCheckboxes, this.note);
+  AddNotePageArguments(
+    this.shouldAddDrawing,
+    this.shouldAddCheckboxes,
+    this.imagePath,
+    this.note,
+  );
 }
